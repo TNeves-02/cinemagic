23c1fd974a0118080452b60d8b8021fc3f39a9c1
David Marques <david.s.marques123@gmail.com>
2022-05-23 14:57:55 +0100

    Teletabies_v2.0

--- resources/views/layouts/basic/footer.blade.php
+++ resources/views/layouts/basic/footer.blade.php
@@ -1,6 +1,6 @@
 
 <!-- Footer -->
-<footer class="fixed-bottom text-center text-white">
+<footer class="text-center text-white">
   <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0);"> Aplicações para Internet - Cinemagic | © 2022 Copyright
 </footer>
 <!-- Footer -->
--- resources/views/layouts/basic/navbar.blade.php
+++ resources/views/layouts/basic/navbar.blade.php
@@ -1,10 +1,10 @@
-<nav class="navbar navbar-expand-lg navbar-light bg-dark">
+<nav class="navbar navbar-expand-lg fixed-top navbar-light bg-dark">
     <img src="{{url('img/navbar/inicial.svg')}}" id="logo-img-navbar">
     &nbsp;&nbsp;&nbsp;&nbsp;
     <a class="navbar-brand mx-center text-white" id="navbar-center" href="#"> Cinemagic </a>
     
     <div class="collapse navbar-collapse justify-content-end" id="navbarSupportedContent">
-    @auth
+        @auth
             <div class="avatar-area">
                 <span class="name-user">{{Auth::user()->name}}</span>
                 <img src="{{Auth::user()->url_foto ? asset('storage/fotos/' . Auth::user()->url_foto) : asset('img/default_img.png') }}">
